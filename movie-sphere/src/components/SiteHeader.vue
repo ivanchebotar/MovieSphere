5f313640cec6ba2dc14a555b4b3410b7b0f264d8
Ivan Chebotar <ic@MBP-Ivan.lan>
2024-12-12 20:46:52 -0500

    first commit

---
+++ movie-sphere/src/components/SiteHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'SiteHeader', // Multi-word component name
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
